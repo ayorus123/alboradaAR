66429ab8bb226f381ad61be8bc4685894a36c197
Christian <clopezbaya@gmail.com>
2020-09-14 13:34:09 -0400

    primera subida de todo

---
+++ Assets/Scripts/Scripts-AR/ScriptAnimations/animation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animation : MonoBehaviour
+{
+    Animation anim;
+    public string nombreAnimacion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+            anim.CrossFade(nombreAnimacion);
+        }
+    }
+}
---
+++ Assets/Scripts/Scripts-AR/VideoController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+    public VideoPlayer vp;
+    //public Texture defaultTex;
+    //public RenderTexture renderTex;
+
+    public void onEnable()
+    {
+        if (vp != null)
+        {
+            vp.Play();
+        }
+    }
+    public void onDisabled()
+    {
+        if (vp != null)
+        {
+            vp.Stop();
+            //Graphics.Blit(defaultTex, renderTex);
+        }
+    }
+
+}
