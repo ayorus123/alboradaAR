cf8d57f42b6a9e91c1f5c2e7a445f19ec81817a9
Christian <clopezbaya@gmail.com>
2020-10-28 06:41:57 -0400

    limpieza de Prefabs y Audio

--- Assets/Scripts/Scripts-AR/DesplegarAudio.cs
+++ Assets/Scripts/Scripts-AR/DesplegarAudio.cs
@@ -2,34 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class animation : MonoBehaviour
+public class DesplegarAudio : MonoBehaviour
 {
-    Animation anim;
-    public string nombreAnimacion;
+    public AudioSource fuente;
+    public AudioClip clip;
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animation>();
+        fuente.clip = clip;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.touchCount == 1 || Input.GetMouseButtonDown(0))
-        {
-      
+    void Update() {
+        if (Input.touchCount == 1 || Input.GetMouseButtonDown(0)) {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
            // Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position)
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
                 if (hit.collider != null)
-                {                                   
-                    anim.CrossFade(nombreAnimacion);
+                {
+                    fuente.Play();
                 }
-                
             }
-            
-        }
+           
+        }       
     }
 }
